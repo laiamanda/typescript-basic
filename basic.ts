371a138744788ee5ca9b68d84dc1094a68847077
laiamanda <40805270+laiamanda@users.noreply.github.com>
2022-12-05 18:41:39 -0500

    Add basic.ts

---
+++ basic.ts
@@ -0,0 +1,66 @@
+// BASIC 1 //
+/* function add(n1: number, n2:number){
+  return n1+ n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result); */
+
+// BASIC 2 //
+/* function add(n1: number, n2:number){
+  if ( typeof n1 !== 'number' || typeof n2 !== 'number'){
+    throw new Error('Incorrect input!');
+  }
+  return n1+ n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result); */
+
+// BASIC 3 //
+// function add(n1: number, n2: number, showResult: boolean, phrase: string){
+//   // if ( typeof n1 !== 'number' || typeof n2 !== 'number'){
+//   //   throw new Error('Incorrect input!');
+//   // }
+//   const result = n1 + n2;
+//   if (showResult){
+//     console.log(phrase +  result);
+//   } else {
+//     return result;
+//   }
+// }
+
+// const number1 = 5; // 5.0
+// const number2 = 2.8;
+// const printResult = true;
+// const resultPhrase = 'Result is: ';
+
+// add(number1, number2, printResult, resultPhrase);
+
+// BASIC 4 //
+// function add(n1: number, n2: number, showResult: boolean, phrase: string){
+//   // if ( typeof n1 !== 'number' || typeof n2 !== 'number'){
+//   //   throw new Error('Incorrect input!');
+//   // }
+//   const result = n1 + n2;
+//   if (showResult){
+//     console.log(phrase +  result);
+//   } else {
+//     return result;
+//   }
+// }
+
+// let number1: number;
+// number1 = 5;
+// const number2 = 2.8;
+// const printResult = true;
+// let resultPhrase = 'Result is: ';
+// // resultPhrase = 5;
+
+// add(number1, number2, printResult, resultPhrase);
