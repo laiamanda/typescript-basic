63010d39e2d882d407e7af897e89f55b0e5152d4
laiamanda <40805270+laiamanda@users.noreply.github.com>
2022-12-06 17:32:23 -0500

    Add app.ts

---
+++ app.js
@@ -0,0 +1,12 @@
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+// userName = userInput;
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An error occurred!', 500);
