6d33141f01e24e31a3fc6b75e1099ebf542bd13e
laiamanda <40805270+laiamanda@users.noreply.github.com>
2022-12-12 19:45:14 -0500

    Add union-aliases.ts

---
+++ union-aliases.ts
@@ -0,0 +1,31 @@
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(
+    input1: Combinable,
+    input2: Combinable,
+    resultConversion: ConversionDescriptor
+    ){
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion ==='as-number'){
+        result = +input1 + +input2; //+ makes it to a number
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+
+    // if( resultConversion === 'as-number'){
+    //     return +result;
+    // } else {
+    //     return result.toString();
+    // }
+    return result;
+}
+
+const combinedAge = combine(30, 26, 'as-number');
+console.log(combinedAge);
+
+const combinedStringsAges = combine('30', '26', 'as-number');
+console.log(combinedStringsAges);
+
+const combinedName = combine('Max', 'Anna', 'as-text');
+console.log(combinedName);
